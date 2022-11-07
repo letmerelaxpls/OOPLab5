abfc3098ec8f6a331ca93e474f8200e749b618ac
Georgiy <larsubivec@gmail.com>
2022-11-07 21:45:24 +0400

    second commit

--- OOPLab5/Program.cs
+++ OOPLab5/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace OOPLab5
 {
+    class Printer
+    {
+        public void Print(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    class PrinterRed : Printer
+    {
+        public void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            base.Print(value);
+        }
+    }
+
+    class PrinterGreen : Printer
+    {
+        public void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            base.Print(value);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Printer printer = new Printer();
+            printer.Print("Приклад 1");
+
+            PrinterRed printerRed = new PrinterRed();
+            printerRed.Print("Приклад 2");
+
+            PrinterGreen printerGreen = new PrinterGreen();
+            printerGreen.Print("Приклад 3");
         }
     }
 }
